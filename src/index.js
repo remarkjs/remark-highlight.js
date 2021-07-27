b28a13d153a6f3198bfcf932c9457efdeb0e8c3a
musayann <musayann@octangrp.com>
2021-07-27 19:50:25 +0200

    feat: add custom language register

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import low from 'lowlight'
 import visit from 'unist-util-visit'
 
-export default function attacher({include, exclude, prefix} = {}) {
+export default function attacher({include, exclude, prefix, languages} = {}) {
+
+  // register custom languages
+  if (languages) {
+    // eslint-disable-next-line guard-for-in
+    for (let name in languages) {
+     low.registerLanguage(name, languages[name])
+    }
+  }
+
   return (ast) => visit(ast, 'code', visitor)
 
   function visitor(node) {
