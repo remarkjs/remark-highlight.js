7cca84f21786eee3f917c36d91773ba8bd9724fe
Titus Wormer <tituswormer@gmail.com>
2021-08-07 11:39:44 +0200

    Update `xo`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import test from 'tape'
 import {remark} from 'remark'
 import remarkHtml from 'remark-html'
